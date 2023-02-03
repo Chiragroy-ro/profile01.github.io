2d62dbef3613f0ef8e956c1a074274276c6521f9
chirag kumar <chiragroy3030@gmail.com>
2023-02-04 00:24:05 +0530

    very first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:portme1/liveapp.dart';
+import 'package:portme1/projectSc.dart';
+import 'package:portme1/socialscreen.dart';
+
+import 'home.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // static const String _title = 'Flutter Code Sample';
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      title: "me",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        extendBodyBehindAppBar: true,
+        body: Screens(),
+      ),
+    );
+  }
+}
+
+
+class Screens extends StatefulWidget {
+  const Screens({super.key});
+
+  @override
+  State<Screens> createState() => _ScreensState();
+}
+
+class _ScreensState extends State<Screens> {
+
+  final PageController controller = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        controller: controller,
+        physics: BouncingScrollPhysics(),
+    
+        children: [
+          SocialScreen(),
+          LiveAppsHere(),
+          
+        ],
+    
+    
+      ),
+    );
+  }
+}
